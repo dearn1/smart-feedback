c4ca60a720a19df4f7580beaadc3a2b0991d1c94
dearn1 <endra.sim@gmail.com>
2025-10-23 15:31:00 +1300

    save add task on rubrics

--- obj/Debug/net8.0/smart-feedback.AssemblyInfo.cs
+++ obj/Debug/net8.0/smart-feedback.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smart-feedback")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59809958cf7aaa74f70713d019de29de89550ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05006c81a9fe8b7957e48628c0084eba8f952da")]
 [assembly: System.Reflection.AssemblyProductAttribute("smart-feedback")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smart-feedback")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
