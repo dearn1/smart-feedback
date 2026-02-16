55abdd259a20ede9f338284d1a1cb80953696b6c
dearn1 <endra.sim@gmail.com>
2026-02-17 10:05:29 +1300

    enhance student module
    1. add column Programme, Year Enrolled, Trimester Enrolled
    2. add search filter by Programme and Trimester

---
+++ Data/Migrations/20260216071009_AddProgrammeYearTrimesterToStudent.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace smart_feedback.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProgrammeYearTrimesterToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Programme",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TrimesterEnrolled",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "YearEnrolled",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Programme",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "TrimesterEnrolled",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "YearEnrolled",
+                table: "Student");
+        }
+    }
+}
